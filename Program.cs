1dc882c8ad0a1eefa203ce0092399811cd007cfe
andika <andikamiri123@gmail.com>
2022-03-22 12:53:50 +0700

    program sederhana taxi dengan class dan object

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi1 = new Taxi();
+
+            taxi1.NameDriver = "Jono";
+            taxi1.OnDuty = true;
+            taxi1.NumPassenger = 10;
+
+            taxi1.TaxiInfo();
+            taxi1.PickUpPassenger();
+            taxi1.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taxi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObject
+{
+    class Taxi
+    {
+        public string NameDriver { get; set; }
+        public Boolean OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", NameDriver);
+
+            if (OnDuty == true) { 
+            Console.WriteLine("OnDuty : yes");
+            }else
+            {
+                Console.WriteLine("OnDuty : no");
+            }
+            Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+            }
+
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine(NameDriver + " sedang menjemput penumpang");
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine(NameDriver + " selesai mengantar penumpang");
+        }
+    }
+}
